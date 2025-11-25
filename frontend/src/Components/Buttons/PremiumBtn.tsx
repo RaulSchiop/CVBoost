8380733acb0c8247b3876ca5a243023782f98ace
RaulSchiop <raul.schiop@gmail.com>
2025-11-25 07:51:30 +0200

    color and btn change

---
+++ frontend/src/Components/Buttons/PremiumBtn.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { motion } from "motion/react";
+
+type Props = {
+   children: React.ReactNode;
+   onClick?: () => void;
+   type?: "submit" | "button";
+};
+
+export default function PremiumBtn({ children, onClick, type }: Props) {
+   return (
+      <motion.button
+         type={type}
+         onClick={onClick}
+         initial={{ opacity: 0, y: -500 }}
+         animate={{ opacity: 1, y: 0 }}
+         transition={{ type: "spring", duration: 0.5 }}
+         whileHover={{
+            scale: 1.05,
+
+            transition: { type: "spring", duration: 0.4 },
+         }}
+         whileTap={{
+            scale: 0.9,
+
+            transition: { type: "spring", duration: 0.4 },
+         }}
+         className="text-white flex items-center justify-center px-5 py-3 gap-4 w-full
+                  bg-premium-500/20 backdrop-blur-md border border-accent-700/30 rounded-xl shadow-lg  hover:bg-premium-500/40    transform active:bg-accent-300"
+      >
+         {children}
+      </motion.button>
+   );
+}
--- frontend/src/Components/Header/SideHeader.tsx
+++ frontend/src/Components/Header/SideHeader.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import NoImage from "../../../public/no image.jpg";
 import { usePathname } from "next/navigation";
 import { motion } from "motion/react";
+import PremiumBtn from "../Buttons/PremiumBtn";
 
 type UserInfo = {
    name: string;
@@ -223,7 +224,26 @@ export default function SideBarHeader() {
                   <p className="text-notUsed-200/60 ">{accInfo.email}</p>
                </div>
             </div>
-            <div className="w-full">
+            <div className="w-full flex flex-col gap-2">
+               <PremiumBtn>
+                  <svg
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     strokeWidth={1.5}
+                     stroke="currentColor"
+                     className="size-6 text-premium-500"
+                  >
+                     <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                     />
+                  </svg>
+
+                  <h1 className="text-premium-500">Upgrade To Pro</h1>
+               </PremiumBtn>
+
                <ContrastBtn>
                   <svg
                      xmlns="http://www.w3.org/2000/svg"
