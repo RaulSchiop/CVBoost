bb03754b5cd627fee7ae9f39cbcf34604d846575
RaulSchiop <raul.schiop@gmail.com>
2025-12-03 14:45:00 +0200

    input structure

--- frontend/src/app/tools/aiReview/page.tsx
+++ frontend/src/app/tools/aiReview/page.tsx
@@ -69,9 +69,32 @@ export default function AiReview() {
                <p>Review Existing</p>
             </MainBtn>
          </div>
-         <div className="bg-contrast-500/40 rounded-2xl mt-5 h-[400px]">
+         <div className="bg-contrast-500/20 rounded-2xl mt-5 h-[400px] backdrop-blur-md ">
             <div className="p-10 w-full h-full">
-               <div className="rounded-2xl border border-gray-500/60 w-full h-full p-5"></div>
+               <div className="rounded-2xl border-2 border-dashed   border-gray-500/80 w-full h-full p-5 flex items-center justify-center">
+                  <div className=" p-2 rounded-xl bg-gradient-to-br from-contrast-500/40 via-contrast-500/20 to-contrast-500/10 border border-gray-500/60">
+                     {/* document writen */}
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-14 text-white/50"
+                     >
+                        <path
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                        />
+                     </svg>
+                  </div>
+                  <div>
+                     <h1 className="text-2xl text-white/80">
+                        Upload Resume PDF
+                     </h1>
+                  </div>
+               </div>
             </div>
          </div>
       </div>
