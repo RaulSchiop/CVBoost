e5a8c394c9a39aeea77088d966aced185e8b0228
RaulSchiop <raul.schiop@gmail.com>
2025-12-05 14:36:12 +0200

    small btn animation changed

--- frontend/src/Components/Buttons/SmallMainBtn.tsx
+++ frontend/src/Components/Buttons/SmallMainBtn.tsx
@@ -5,15 +5,23 @@ type Props = {
    children: React.ReactNode;
    onClick?: () => void;
    type?: "submit" | "button";
+   color?: string;
+   ClassName?: string;
 };
 
-export default function SmallBtn({ children, onClick, type }: Props) {
+export default function SmallBtn({
+   children,
+   onClick,
+   type,
+   color = "bg-accent-700/50",
+   ClassName,
+}: Props) {
    return (
       <motion.button
          type={type}
          onClick={onClick}
-         initial={{ opacity: 0, y: -500 }}
-         animate={{ opacity: 1, y: 0 }}
+         initial={{ opacity: 0 }}
+         animate={{ opacity: 1 }}
          transition={{ type: "spring", duration: 0.5 }}
          whileHover={{
             scale: 1.1,
@@ -25,8 +33,8 @@ export default function SmallBtn({ children, onClick, type }: Props) {
 
             transition: { type: "spring", duration: 0.4 },
          }}
-         className="text-white flex items-center justify-between px-2 py-1 gap-3
-                  bg-accent-700/50 backdrop-blur-md border border-accent-700/30 rounded-xl shadow-lg  active:bg-accent-300"
+         className={`text-white flex items-center justify-between px-2 py-1 gap-3 ${color} 
+                   backdrop-blur-md border border-accent-700/30 rounded-xl shadow-lg  active:bg-accent-300 ${ClassName}`}
       >
          {children}
       </motion.button>
