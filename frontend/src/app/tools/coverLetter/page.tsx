ef7094f7aaab944eb3d26c96e0459c5657707a93
RaulSchiop <raul.schiop@gmail.com>
2025-12-20 12:44:26 +0200

    staps for next ui

--- frontend/src/app/tools/coverLetter/page.tsx
+++ frontend/src/app/tools/coverLetter/page.tsx
@@ -1,6 +1,12 @@
 export default function CoverLetter(){
 
     return(
-        <h1 className="text-white">asdasd</h1>
+        <div>
+            {/* stap 1 */}
+            {/* upload a job description,job title and a cv to know the person */}
+
+            {/* stap 2 */}
+            {/* based on the cv ( that person ) and job description and title create a cover letter ( create a txt file and download it) */}
+        </div>
     )
 }
--- frontend/src/app/tools/transform/page.tsx
+++ frontend/src/app/tools/transform/page.tsx
@@ -1,6 +1,20 @@
 export default function JobMetch(){
 
     return (
-        <h1 className="text-white">sadasdasd</h1>
+        <div>
+            <div>
+                {/* title and icon*/}
+            </div>
+                {/* stap 1 */}
+                {/* chose select old or upload */}
+                {/* button to start mataching (cool loading skeleton)*/}
+
+                {/* stap 2 */}
+                {/* what s wrong and how to improve the cv to match that */}
+
+                {/* stap 3 */}
+                {/* update the cv and download it */}
+                
+        </div>
     )
 }
