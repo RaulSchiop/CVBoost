610df86dad54ecdd5a610b4228d151bd32c3cc5d
RaulSchiop <raul.schiop@gmail.com>
2025-12-20 13:03:16 +0200

    create cv page

--- frontend/src/app/tools/createResume/page.tsx
+++ frontend/src/app/tools/createResume/page.tsx
@@ -1,7 +1,47 @@
 export default function CreateResume() {
    return (
       <div>
-         <h1 className="text-white">Create</h1>
+         {/* stap 1 */}
+         {/* upload in inputs and create cv */}
+
+         {/* inputs : */}
+         {/* header
+         - name
+         - email
+         - phone number
+         - ?github
+         - ?linkedin Link
+         - location
+
+         - summery sections
+
+         - skills section (more complex)
+
+         - experience 
+            - ex title 
+            - period
+            - description
+         add more experience
+
+         - projects
+            - p Name
+            - period 
+            - description
+         add more projects
+         tick to add se more projects if more
+
+         education
+            tips to put the university justify
+            - name
+            - period 
+            - degree
+ */}
+
+      
+
+         {/* stap 2 */}
+         {/* button to generate */}
+         {/* view and or download cv ( save to db ) */}
       </div>
    );
 }
