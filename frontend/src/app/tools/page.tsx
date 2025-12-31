1492a289456ccb0d91a139e4656bdf8087fa99d2
RaulSchiop <raul.schiop@gmail.com>
2025-12-31 11:40:13 +0200

    dashboard

--- frontend/src/app/tools/page.tsx
+++ frontend/src/app/tools/page.tsx
@@ -1,16 +1,158 @@
 "use client";
+import MainCard from "@/Components/Cards/MainCard";
+import List from "@/Components/List/ClasicList";
 import Title from "@/Components/Text/Title";
 import { motion } from "motion/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { JSX } from "react";
 
 export default function Tools() {
+   const pathname = usePathname();
+   const paths: {
+      icon: JSX.Element;
+      link: string;
+      label: string;
+      description: string;
+   }[] = [
+      {
+         icon: (
+            /* document writen */
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               strokeWidth={1.5}
+               stroke="currentColor"
+               className={`w-8 h-8  text-white`}
+            >
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+               />
+            </svg>
+         ),
+         link: "/tools/resumes",
+         label: "My Resumes",
+         description:
+            "View and manage all your uploaded resumes in one place. You can also create a new resume using the “Create New Resume” button.",
+      },
+      {
+         icon: (
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               strokeWidth={1.5}
+               stroke="currentColor"
+               className={`w-8 h-8  text-white`}
+            >
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+               />
+            </svg>
+         ),
+         link: "/tools/coverLetter",
+         label: "Cover Letters",
+         description:
+            "View your existing cover letters or create a new one tailored to a specific job using your resume and job details.",
+      },
+      {
+         icon: (
+            //suit case icon
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               strokeWidth={1.5}
+               stroke="currentColor"
+               className={`w-8 h-8  text-white`}
+            >
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"
+               />
+            </svg>
+         ),
+         link: "/tools/transform",
+         label: "Job Match",
+         description:
+            "Upload or select a resume, add job details, and receive a match score with suggestions to improve your CV using AI.",
+      },
+      {
+         icon: (
+            //stars icon
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               strokeWidth={1.5}
+               stroke="currentColor"
+               className={`w-8 h-8  text-white`}
+            >
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+               />
+            </svg>
+         ),
+         link: "/tools/aiReview",
+         label: "Ai Review",
+         description:
+            "Upload or select a resume to receive an ATS score along with AI-powered tips to improve structure, clarity, and effectiveness.",
+      },
+   ];
+
    return (
       <div className="w-full min-h-screen p-10 md:p-15 bg-gradient-to-bl from-contrast-500/20 via-contrast-500/60 to-contrast-500/20">
          <Title title=" Dashboard">
             {" "}
-            Dashboard for all your
+            Dashboard that describes all your
             <a className="text-accent-500 text-xl font-bold"> AI</a>-powered
             tools.
          </Title>
+         <List>
+            {paths.map((items, index) => (
+               <motion.li
+                  initial={{ opacity: 0, x: -200 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{
+                     type: "spring",
+                     duration: 1,
+                     delay: index * 0.2,
+                  }}
+                  whileHover={{
+                     scale: 1.05,
+
+                     transition: { type: "spring", duration: 0.4 },
+                  }}
+                  whileTap={{
+                     scale: 0.9,
+
+                     transition: { type: "spring", duration: 0.4 },
+                  }}
+                  key={index}
+               >
+                  <MainCard>
+                     <Link
+                        href={items.link}
+                        className="flex items-center gap-4 px-3 py-4  flex-col"
+                     >
+                        <div>{items.icon}</div>
+                        <h1 className="text-white text-2xl">{items.label}</h1>
+                        <p className="text-white/70 text-center">
+                           {items.description}
+                        </p>
+                     </Link>
+                  </MainCard>
+               </motion.li>
+            ))}
+         </List>
       </div>
    );
 }
