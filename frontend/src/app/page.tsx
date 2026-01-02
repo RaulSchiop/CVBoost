5cefd5811249c94527f136f72b8955ce71c390e6
RaulSchiop <raul.schiop@gmail.com>
2026-01-02 13:13:29 +0200

    hero page design startr

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,9 +9,9 @@ import Link from "next/link";
 export default function Home() {
    return (
       <div className="min-h-screen flex flex-col overflow-x-hidden">
-         <div className="flex flex-col items-center pt-30 gap-6 p-10 min-h-screen bg-gradient-to-bl from-accent-200 via-contrast-500/50 to-accent-200">
+         <div className="flex flex-col items-center pt-30 gap-6 p-10 min-h-screen ">
             <Header></Header>
-            <div className="flex items-center justify-center flex-col gap-5">
+            <div className="flex items-center justify-center flex-col gap-5 bg-gradient-to-bl from-black via-accent-500/50 to-black w-full h-[500px] rounded-3xl">
                <Title title="Boost Your CV with AI">
                   <p className="text-xl mt-2 font-bold w-2xl text-center bg-gradient-to-r from-accent-900 via-accent-700 to-accent-900 bg-clip-text text-transparent">
                      Create job-winning CVs and personalized cover letters in
