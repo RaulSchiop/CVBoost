af00ecd1894a798a4b66c4586d07fcbbd6080ee3
RaulSchiop <raul.schiop@gmail.com>
2026-02-13 13:03:00 +0200

    interview asistent frontend done

--- frontend/src/Components/InputsComponents/interviewAsistent.tsx
+++ frontend/src/Components/InputsComponents/interviewAsistent.tsx
@@ -1,6 +1,47 @@
+"use client";
+import { useState } from "react";
+import MainBtn from "../Buttons/MainBtn";
+
 export default function InterviewAsistentInput() {
-return(
-    <div></div>
-)
-    
+   const [inputData, setInputData] = useState({
+      seniority: "",
+      description: "",
+   });
+
+   const handleChange = (
+      e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+   ) => {
+      e.preventDefault();
+      const { value, name } = e.target;
+      setInputData((prev) => ({
+         ...prev,
+         [name]: value,
+      }));
+   };
+   console.log(inputData);
+
+   return (
+      <div className="w-[80%] mt-10">
+         <form
+            className="flex flex-col gap-4 mt-5"
+            onSubmit={() => console.log("to do")}
+         >
+            <input
+               placeholder="Seniority ( Junior or Mid or Senior )"
+               name="seniority"
+               type="text"
+               onChange={handleChange}
+               required
+               className="text-white placeholder-white/60 bg-contrast-500/20 border border-gray-500/60 focus:border-accent-500 rounded px-4 py-2 focus:outline-none"
+            />
+            <textarea
+               name="description"
+               onChange={handleChange}
+               placeholder="Job Description "
+               className="text-white placeholder-white/60 bg-contrast-500/20 border border-gray-500/60 focus:border-accent-500 rounded px-4 py-2 focus:outline-none"
+            ></textarea>
+            <MainBtn>Generate</MainBtn>
+         </form>
+      </div>
+   );
 }
--- frontend/src/app/tools/interview/page.tsx
+++ frontend/src/app/tools/interview/page.tsx
@@ -1,15 +1,16 @@
+import InterviewAsistentInput from "@/Components/InputsComponents/interviewAsistent";
 import Title from "@/Components/Text/Title";
 
 export default function Learning() {
    return (
-      <div className="min-h-screen bg-gradient-to-bl from-contrast-500/20 via-contrast-500/60 to-contrast-500/20 p-5 lg:p-20">
+      <div className="min-h-screen bg-gradient-to-bl flex items-center justify-center flex-col from-contrast-500/20 via-contrast-500/60 to-contrast-500/20 p-5 lg:p-20">
          <Title title="Interview Asistent">
             <p className="w-xl text-center">
                Fill in the details below to generate a tailored learning roadmap
                and 10 interview questions with answers.
             </p>
          </Title>
-         
+         <InterviewAsistentInput></InterviewAsistentInput>
       </div>
    );
 }
